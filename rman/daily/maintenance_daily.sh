7654b55451f1f1772c3d9a8189e6ec9aeac638d3
cyancaesar <29235530+cyancaesar@users.noreply.github.com>
2026-02-08 15:05:08 +0000

    add bash scripts

--- rman/daily/maintenance_daily.sh
+++ rman/daily/maintenance_daily.sh
@@ -1,5 +1,38 @@
 #! /bin/sh
 
-set -e
+export ORACLE_HOME=/u01/app/oracle/products/23.26.1/db_home1
+export ORACLE_SID=cyandb01
+export PATH=$ORACLE_HOME/bin:$PATH
 
-nohup rman target / cmdfile=/backup/rman/scripts/maintenance_daily.rman log=/backup/rman/logs/maintenance_daily_$(date +%Y_%m_%d_%s).log >/dev/null 2>&1
+readonly DATE_NOW=$(date +%Y_%m_%d_%S)
+readonly BASE_DIR=/backup/rman
+readonly SCRIPT_DIR=${BASE_DIR}/scripts
+readonly LOG_DIR=${BASE_DIR}/scripts
+readonly RMAN_FILE=${SCRIPT_DIR}/daily/maintenance.daily.rman
+readonly LOG_FILE=${BASE_DIR}/logs/maintenance.daily.${DATE_NOW}.log
+
+# Check if the user is in dba group
+id -nG $USER | grep -q dba
+if [ $? -ne 0 ]; then
+  echo "The user $USER must be in dba group"
+  exit 1
+fi
+
+# Check if the RMAN file exists
+if [ ! -f ${RMAN_FILE} ]; then
+  echo "The RMAN file ${RMAN_FILE} does not exists!"
+  exit 1
+fi
+
+# Running RMAN
+rman target / \
+  cmdfile=${RMAN_FILE} \
+  log=${LOG_FILE}
+  
+if [ $? -eq 0 ]; then
+  echo "RMAN daily maintenance script finished successfully.";
+  exit 0
+else
+  echo "RMAN daily maintenance script failed.";
+  exit 1
+fi
--- rman/weekly/level0_weekly.sh
+++ rman/weekly/level0_weekly.sh
@@ -1,5 +1,38 @@
 #! /bin/sh
 
-set -e
+export ORACLE_HOME=/u01/app/oracle/products/23.26.1/db_home1
+export ORACLE_SID=cyandb01
+export PATH=$ORACLE_HOME/bin:$PATH
 
-nohup rman target / cmdfile=/backup/rman/scripts/level0_weekly.rman log=/backup/rman/logs/level0_weekly_$(date +%Y_%m_%d_%s).log >/dev/null 2>&1
+readonly DATE_NOW=$(date +%Y_%m_%d_%S)
+readonly BASE_DIR=/backup/rman
+readonly SCRIPT_DIR=${BASE_DIR}/scripts
+readonly LOG_DIR=${BASE_DIR}/scripts
+readonly RMAN_FILE=${SCRIPT_DIR}/weekly/level0.weekly.rman
+readonly LOG_FILE=${BASE_DIR}/logs/level0.weekly.${DATE_NOW}.log
+
+# Check if the user is in dba group
+id -nG $USER | grep -q dba
+if [ $? -ne 0 ]; then
+  echo "The user $USER must be in dba group"
+  exit 1
+fi
+
+# Check if the RMAN file exists
+if [ ! -f ${RMAN_FILE} ]; then
+  echo "The RMAN file ${RMAN_FILE} does not exists!"
+  exit 1
+fi
+
+# Running RMAN
+rman target / \
+  cmdfile=${RMAN_FILE} \
+  log=${LOG_FILE}
+  
+if [ $? -eq 0 ]; then
+  echo "RMAN weekly level 0 backup script  finished successfully.";
+  exit 0
+else
+  echo "RMAN weekly level 0 backup script failed.";
+  exit 1
+fi
